2943e7c8683567ddbbc125a4aa8b962b66900b8c
adellewigley <aw1n15@soton.ac.uk>
2017-04-23 21:16:23 +0100

    edits
    images
    ghost icon
    html text

--- static/js/map.js
+++ static/js/map.js
@@ -2,19 +2,22 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiYWRlbGxlIiwiYSI6ImNqMW5vZDVwMzAwMHozM21uMXpmb
         
 var map = L.mapbox.map('map','mapbox.dark').setView([50.9208, -1.4050], 13);
 
+var ghost = L.icon({
+    iconUrl: './static/img/ghost.png',
+});
 
-var places = new L.LayerGroup();
+        L.marker([50.898970, -1.403749], {icon: ghost})
+        	.addTo(map).bindPopup('The Red Lion Pub');
+        
+        L.marker([50.918020, -1.375211], {icon: ghost}) 
+       		.addTo(map).bindPopup('The Station Pub');
+        
+        L.marker([50.898566, -1.405247], {icon: ghost})
+            .addTo(map).bindPopup('The Medieval Merchants');
+        
+        L.marker([50.899590, -1.405917], {icon: ghost})
+            .addTo(map).bindPopup('The Tudor House Museum');
 
-        L.marker([50.898970, -1.403749])
-            .bindPopup('The Red Lion Pub').addTo(places),
-        L.marker([50.918020, -1.375211])
-            .bindPopup('The Station Pub').addTo(places),
-        L.marker([50.898566, -1.405247])
-            .bindPopup('The Medieval Merchants').addTo(places),
-        L.marker([50.899590, -1.405917])
-            .bindPopup('The Tudor House Museum').addTo(places);
-
-map.addLayer(places);
 
 
 
