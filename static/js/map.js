1e52939a534960cc970e0c21dda7925d9db8d85a
adellewigley <aw1n15@soton.ac.uk>
2017-04-26 18:19:59 +0100

    edits
    css - images

--- static/js/map.js
+++ static/js/map.js
@@ -2,20 +2,17 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiYWRlbGxlIiwiYSI6ImNqMW5vZDVwMzAwMHozM21uMXpmb
         
 var map = L.mapbox.map('map','mapbox.dark').setView([50.9208, -1.4050], 13);
 
-var ghost = L.icon({
-    iconUrl: './static/img/ghost.png',
-});
 
-        L.marker([50.898970, -1.403749], {icon: ghost})
+        L.marker([50.898970, -1.403749])
         	.addTo(map).bindPopup('The Red Lion Pub');
         
-        L.marker([50.918020, -1.375211], {icon: ghost}) 
+        L.marker([50.918020, -1.375211]) 
        		.addTo(map).bindPopup('The Station Pub');
         
-        L.marker([50.898566, -1.405247], {icon: ghost})
+        L.marker([50.898566, -1.405247])
             .addTo(map).bindPopup('The Medieval Merchants');
         
-        L.marker([50.899590, -1.405917], {icon: ghost})
+        L.marker([50.899590, -1.405917])
             .addTo(map).bindPopup('The Tudor House Museum');
 
 
